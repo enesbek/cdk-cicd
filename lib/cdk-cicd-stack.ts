af614ddbe43547b18b4c6ab9777c4980f8937d8e
enesbek <enes.bek.93@gmail.com>
2024-05-29 23:31:25 +0300

    test pipeline

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -14,7 +14,17 @@ export class CdkCicdStack extends cdk.Stack {
       pipelineName: "TestPipeline",
       synth: new ShellStep("Synth", {
         input: CodePipelineSource.gitHub("enesbek/cdk-cicd", "dev"),
-        commands: ["npm ci", "npx cdk synth"],
+        commands: [
+          "echo 'Current directory before cd'",
+          "pwd",
+          "echo 'Listing contents before cd'",
+          "ls -la",
+          "cd cdk-cicd",
+          "echo 'Changed directory to cdk-cicd'",
+          "pwd",
+          "npm ci",
+          "npx cdk synth",
+        ],
         primaryOutputDirectory: "cdk-cicd/cdk.out",
       }),
     });
