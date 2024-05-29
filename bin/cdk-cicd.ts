f343f741d51a116d271e020eccb3dae673de7a1b
enesbek <enes.bek.93@gmail.com>
2024-05-29 23:21:55 +0300

    test commit

--- bin/cdk-cicd.ts
+++ bin/cdk-cicd.ts
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CdkCicdStack } from '../lib/cdk-cicd-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { CdkCicdStack } from "../lib/cdk-cicd-stack";
 
 const app = new cdk.App();
-new CdkCicdStack(app, 'CdkCicdStack', {
-  
+new CdkCicdStack(app, "CdkCicdStack", {
+  env: {
+    account: "975050224235",
+    region: "eu-central-1",
+  },
 });
