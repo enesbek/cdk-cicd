aada74c992fcaf7349f12df8a42c0c362ea839ff
enesbek <enes.bek.93@gmail.com>
2024-05-30 12:08:55 +0300

    test

--- test/cdk-cicd.test.ts
+++ test/cdk-cicd.test.ts
@@ -3,6 +3,6 @@ import { handler } from "../services/hello";
 describe("initial test suit", () => {
   test("hello handler should return 200", async () => {
     const result = await handler({}, {});
-    expect(result.statusCode).toBe(200);
+    expect(result.statusCode).toBe(300);
   });
 });
